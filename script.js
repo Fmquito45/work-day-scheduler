f3ed21f91101c899cae46097941a95a0dc5e372c
Fmquito45 <fmquitox45@gmail.com>
2023-09-09 11:34:03 -0400

    adds code to display current date on calender

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-// $(function () {
+
+
+ $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -20,6 +22,10 @@
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-// });
+  //WHEN I open the planner
+  //THEN the current day is displayed at the top of the calendar
+  var currentData = dayjs().format('dddd, MMMM D, YYYY');
+  $('#currentDay').text(currentData);
+ });
 
 
